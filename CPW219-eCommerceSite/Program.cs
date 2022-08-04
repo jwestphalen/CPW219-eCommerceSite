35bae1b20ea174f1e88e711cefcdda7c80e060fe
Joshua Westphalen <joshua.westphalen@yahoo.com>
2022-08-03 21:19:16 -0700

    Configured DB Context

--- CPW219-eCommerceSite/Program.cs
+++ CPW219-eCommerceSite/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<CPW219_eCommerceSite.Data.VideoGameContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
