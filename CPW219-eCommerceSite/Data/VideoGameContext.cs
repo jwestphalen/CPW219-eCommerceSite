414c56b89ba9293d1cca0dc38776c4a514c4885a
Joshua Westphalen <joshua.westphalen@yahoo.com>
2022-08-03 17:59:56 -0700

    Added DbContext Class

--- CPW219-eCommerceSite/Data/VideoGameContext.cs
+++ CPW219-eCommerceSite/Data/VideoGameContext.cs
@@ -1,7 +1,15 @@
-﻿namespace CPW219_eCommerceSite.Data
+﻿using CPW219_eCommerceSite.Models;
+using Microsoft.EntityFrameworkCore;
+namespace CPW219_eCommerceSite.Data
 {
-    public class VideoGameContext
+    public class VideoGameContext : DbContext
     {
+        public VideoGameContext(DbContextOptions<VideoGameContext> options)
+            : base(options)
+        {
 
+        }
+
+        public DbSet<Game> Games { get; set; }
     }
 }
